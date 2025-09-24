a861d456184bff4ec288d450d031978bd6cb4608
Salla0905 <bruna.salla@germinare.org.br>
2025-09-24 09:17:03 -0300

    initial commit

---
+++ ms-sinara-mobile/src/main/java/com/example/mssinaramobile/MsSinaraMobileApplication.java
@@ -0,0 +1,13 @@
+package com.example.mssinaramobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSinaraMobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSinaraMobileApplication.class, args);
+	}
+
+}
---
+++ ms-sinara-mobile/src/test/java/com/example/mssinaramobile/MsSinaraMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mssinaramobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSinaraMobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
